d6ed7dbdd5dcae1d0bfa388fee43927e675b3d41
Ahmed Madian <ahmed.mostafa.madian@gmail.com>
2025-04-29 13:05:27 +0200

    Initial Commit

---
+++ MarktGuru/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarktGuru
+//
+//  Created by Ahmed Madian on 29.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarktGuru/MarktGuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarktGuruApp.swift
+//  MarktGuru
+//
+//  Created by Ahmed Madian on 29.04.25.
+//
+
+import SwiftUI
+
+@main
+struct MarktGuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
